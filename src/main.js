fdbcc37f6f10954b6955375cbe9eaa51b745a287
tang <979472293@qq.com>
2019-10-13 20:16:16 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,36 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './style/index.less'
+import { Button,
+  NavBar,
+  Field,
+  Toast
+} from 'vant'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 加载验证插件的语言包
+import * as rules from 'vee-validate/dist/rules'
+Vue.use(Button)
+  .use(NavBar)
+  .use(Field)
+  .use(Toast)
+// 配置使用中文语言
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+// 注册校验组件
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+// 自定义扩展规则
+extend('phone', {
+  validate: function (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  message: '手机号格式错误'
+})
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,99 @@
+<template>
+<div class="login">
+    <!-- 导航栏 -->
+    <van-nav-bar
+    title="标题"
+    />
+    <!-- 登录表单 -->
+    <ValidationObserver ref="loginForm">
+          <van-cell-group>
+            <ValidationProvider name='手机号' rules='required|phone' v-slot='{errors}'>
+                <van-field
+                  required
+                  v-model="user.mobile"
+                  clearable
+                  label="手机号"
+                  placeholder="请输入手机号"
+                  :error-message='errors[0]'
+                />
+            </ValidationProvider>
+
+            <ValidationProvider name='验证码' rules='required|max:6' v-slot='{errors}'>
+              <van-field
+              v-model="user.code"
+                label="验证码"
+                placeholder="请输入验证码"
+                required
+                :error-message='errors[0]'
+              />
+            </ValidationProvider>
+
+          </van-cell-group>
+    </ValidationObserver>
+    <!-- 登录按钮 -->
+    <div class="btn-wrap">
+      <van-button type="info" class="btn" @click="onLogin">登录</van-button>
+    </div>
+</div>
+</template>
+
+<script>
+import { login } from '@/api/user.js'
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 表单验证
+      const isValid = await this.$refs.loginForm.validate()
+      if (!isValid) {
+        return
+      }
+      const toast = this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        forbidClick: true, // 禁用背景点击
+        loadingType: 'spinner',
+        message: '登录中'
+      })
+      try {
+        // 请求提交表单数据
+        // const { data } = await request({
+        //   method: 'POST',
+        //   url: '/app/v1_0/authorizations',
+        //   data: this.user
+        // })
+
+        const { data } = await login(this.user)
+        this.$store.commit('setUser', data.data)
+        toast.clear()
+        this.$toast.success('登录成功')
+      } catch (err) {
+        if (err.response && err.response.status === 400) {
+          toast.clear()
+          this.$toast.fail('登录失败，手机号或验证码错误')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+  .login {
+    .btn-wrap {
+      padding: 20px;
+      .btn {
+        width: 100%;
+        background-color: #6db4fb;
+        color: #fff;
+      }
+  }
+  }
+</style>
